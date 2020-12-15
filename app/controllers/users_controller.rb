77d674e99d129346d768efef39fd0e82b6c2c323
EC2 Default User <ec2-user@ip-172-31-13-132.us-east-2.compute.internal>
2020-12-15 14:32:54 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
   def new
     @user = User.new
   end
+  
   def create
      @user = User.new(user_params)
     if @user.save
